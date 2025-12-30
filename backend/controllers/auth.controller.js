c3d5463c79ea00921017deef842f2a351fcb4a61
Zain-mahfoud94 <zainmahfoud14@gmail.com>
2025-12-31 00:46:35 +0100

    Backend. Signup endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,48 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+
 export const signup = async (req, res) => {
-  res.send("Signup route");
+  const { email, password, name } = req.body;
+  try {
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists)
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const verificationToken = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+    const user = await User.insertOne({
+      email,
+      password: hashedPassword,
+      name,
+      verificationToken: verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, // 24 Hours
+    });
+    // Save the user
+    await user.save();
+    // JWT
+    generateTokenAndSetCookie(res, user._id);
+    // Send the reponse
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: { ...user._doc, password: undefined },
+    });
+  } catch (error) {
+    console.log("Failed to signup with error: ", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error. Please try again later",
+    });
+  }
 };
 
 export const login = async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+// Middelware
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
